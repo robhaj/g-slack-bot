6abb4661679a771a9660423cb71238aa99c50401
Robert Hajek <hajek.rob@gmail.com>
2016-08-01 15:45:34 -0600

    include botkit

---
+++ README.md
@@ -0,0 +1,8 @@
+# botBelly
+This is a slack bot intended to help with food ordering and delivery.
+
+# Under The Hood
+- Slack API (interface)
+- Locu API (menus)
+- Postmates API (delivery)
+- Stripe API (payment)
---
+++ bot.js
@@ -0,0 +1,19 @@
+var request = require('request-promise')
+var BotKit = require('botkit')
+
+var gBot = BotKit.slackbot({
+  debug: false
+});
+
+// Connect to Slack via BeepBoop
+gBot.spawn({
+  token: process.env.SLACK_TOKEN
+}).startRTM(function(err,bot,payload){
+  if (err) {
+    throw new Error('Could not connect to slack');
+  }
+})
+
+gBot.hears('Hi', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  bot.reply('Please link all relevant curriculum resources to Git')
+});
